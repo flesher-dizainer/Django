af44e50fd56f32f3d276ed065f4596b6f878a73d
flesher <56498635+flesher-dizainer@users.noreply.github.com>
2024-12-04 21:24:36 +0300

    Add

--- blog/urls.py
+++ blog/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from python_blog import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.main),
 ]
