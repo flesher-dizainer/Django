b4dfc93ae9789ee4db18e86c2bb76eb73d6fbdef
flesher <56498635+flesher-dizainer@users.noreply.github.com>
2024-12-14 16:17:00 +0300

    hw_21. Cоздание представлений. Создание шаблонов.

--- python_blog/views.py
+++ python_blog/views.py
@@ -1,5 +1,43 @@
-from  django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-def main(request)->HttpResponse:
-    return HttpResponse("Hello World")
+from django.shortcuts import render
+#from django.http import HttpResponse
+
+CATEGORIES = [
+    {'slug': 'python', 'name': 'Python'},
+    {'slug': 'django', 'name': 'Django'},
+    {'slug': 'postgresql', 'name': 'PostgreSQL'},
+    {'slug': 'docker', 'name': 'Docker'},
+    {'slug': 'linux', 'name': 'Linux'},
+]
+POSTS = [
+    {'slug': 'blog', 'name': 'Blog'},
+    {'slug': 'home', 'name': 'Home'},
+    {'slug': 'about', 'name': 'About'},
+    {'slug': 'contact', 'name': 'Contact'},
+    {'slug': 'privacy', 'name': 'Privacy'},
+]
+TAGS=[
+    {'slug': 'Div', 'name': 'Div'},
+
+]
+def main(request):
+    return render(request, 'python_blog/main.html')
+
+def catalog_posts(request):
+    posts = POSTS
+    return render(request, 'python_blog/catalog_posts.html', {'posts': posts})
+
+def catalog_categories(request):
+    categories = CATEGORIES  # Список категорий
+    return render(request, 'python_blog/catalog_categories.html', {'categories': categories})
+
+def category_detail(request, category_slug):
+    return render(request, 'python_blog/category_detail.html', {'category_slug': category_slug})
+
+def catalog_tags(request):
+    return render(request, 'python_blog/catalog_tags.html',{'tags': TAGS})
+
+def tag_detail(request, tag_slug):
+    return render(request, 'python_blog/tag_detail.html', {'tag_slug': tag_slug})
+
+def post_detail(request, post_slug):
+    return render(request, 'python_blog/post_detail.html', {'post_slug': post_slug})
