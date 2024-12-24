2156882f296c3b70fcb753f11f2125c93fac7939
flesher <56498635+flesher-dizainer@users.noreply.github.com>
2024-12-24 23:24:07 +0300

    Добавление menu_items в context_processors.py для передачи во views menu_items в контекст по умолчанию.

--- python_blog/context_processors.py
+++ python_blog/context_processors.py
@@ -1,12 +1,13 @@
-MENU_ITEMS = [
-    {"title": "Главная", "url_name": "main"},
-    {"title": "Блог", "url_name": "blog:blog"},
-    {"title": "Категории", "url_name": "blog:catalog_categories"},
-    {"title": "Теги", "url_name": "blog:catalog_tags"},
-    {"title": "About", "url_name": "about"},
-]
-
 def menu_items(request):
+    MENU_ITEMS = [
+        {"title": "Главная", "url_name": "main"},
+        {"title": "Блог", "url_name": "blog:blog"},
+        {"title": "Категории", "url_name": "blog:catalog_categories"},
+        {"title": "Теги", "url_name": "blog:catalog_tags"},
+        {"title": "О нас", "url_name": "about"},
+    ]
+    for item in MENU_ITEMS:
+        item['is_active'] = request.resolver_match.view_name == item['url_name']
     return {
         'menu_items': MENU_ITEMS,
     }
