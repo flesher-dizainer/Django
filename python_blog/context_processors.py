576a1ad811c12c8b5204a4635b121f3d64b7923e
flesher <56498635+flesher-dizainer@users.noreply.github.com>
2025-02-13 10:59:45 +0300

    HW28. Удаление кнопок категории и тэги

--- python_blog/context_processors.py
+++ python_blog/context_processors.py
@@ -2,8 +2,6 @@ def menu_items(request):
     MENU_ITEMS = [
         {"title": "Главная", "url_name": "main"},
         {"title": "Блог", "url_name": "blog:blog"},
-        {"title": "Категории", "url_name": "blog:catalog_categories"},
-        {"title": "Теги", "url_name": "blog:catalog_tags"},
         {"title": "О нас", "url_name": "about"},
     ]
     for item in MENU_ITEMS:
