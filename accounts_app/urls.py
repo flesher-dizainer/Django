5d3a220fac6b0e349316f20646581724d9dec951
flesher <56498635+flesher-dizainer@users.noreply.github.com>
2025-02-14 11:14:41 +0300

    HW28. Модернизация путей для работы с профилем.

--- accounts_app/urls.py
+++ accounts_app/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path
-from .views import signup_view, login_view, CustomLogoutView, profile_view
+from . import views
 
 app_name = 'accounts_app'
 
 urlpatterns = [
-    path('signup/', signup_view, name='signup'),
-    path('login/', login_view, name='login'),
-    path('logout/', CustomLogoutView.as_view(), name='logout'),
-    path('profile/', profile_view, name='profile'),
+    path('signup/', views.signup_view, name='signup'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.CustomLogoutView.as_view(), name='logout'),
+    path('profile/', views.profile_view, name='profile'),  # базовый профиль
+    path('profile/<str:username>/', views.profile_view, name='user_profile'),  # профиль по username
+    path('change-password/', views.change_password, name='change_password'),
+    path('password-reset/', views.CustomPasswordResetView.as_view(), name='password_reset'),
+    path('password-reset/done/', views.CustomPasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password-reset/<uidb64>/<token>/', views.CustomPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password-reset/complete/', views.CustomPasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
