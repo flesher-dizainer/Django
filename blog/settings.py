cb21b3aed4c669d8ae2a29eb57b8169c41995da8
flesher <56498635+flesher-dizainer@users.noreply.github.com>
2025-02-14 11:15:13 +0300

    HW28. Доработка.

--- blog/settings.py
+++ blog/settings.py
@@ -138,3 +138,10 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# Настройки email (пример для Gmail)
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'your-email@gmail.com'  # Ваш email
+EMAIL_HOST_PASSWORD = 'your-app-password'  # Пароль приложения
